ce6f2f8e728d93b7e0230a002c7d514e7b46177f
KMS322 <kms930322@naver.com>
2023-12-29 14:40:40 +0900

    231229_11

--- front/src/constants.js
+++ front/src/constants.js
@@ -1,3 +1,3 @@
 // export const API_URL = "http://localhost:3065";
 // export const API_URL = "http://52.79.149.76";
-export const API_URL = "http://52.78.107.42";
+export const API_URL = "http://52.78.107.42:3000";
