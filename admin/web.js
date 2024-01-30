1bd9f4e9222d29630201eaf0bb2c70f5209618d9
KMS322 <kms930322@naver.com>
2024-01-30 09:31:49 +0900

    admin web add cors

--- admin/web.js
+++ admin/web.js
@@ -1,9 +1,20 @@
 const express = require("express");
+const cors = require("cors");
 const path = require("path");
 const app = express();
 // const port = process.env.PORT || 3000;
 const port = 3000;
 // const port = 3306;
+
+app.use(
+  cors({
+    origin: ["http://www.accydream.com:3060"],
+    credentials: true,
+  })
+);
+
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
 
 app.use(express.static(path.join(__dirname, "build")));
 
