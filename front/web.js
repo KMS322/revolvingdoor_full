530cd79293fd0f8c10ed8043d076e5ffb58997a2
KMS322 <kms930322@naver.com>
2024-02-20 10:16:25 +0900

    web.js change

--- front/web.js
+++ front/web.js
@@ -7,7 +7,7 @@ const port = process.env.PORT || 80;
 app.use(express.static(path.join(__dirname, "build")));
 
 app.get("/*", function (req, res) {
-  res.sendFile(path.join(__dirname, "build", "index.html"));
+  res.status(200).sendFile(path.join(__dirname, "build", "index.html"));
 });
 
 app.listen(port, () => {
