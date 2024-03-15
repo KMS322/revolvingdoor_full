367210377b83cffe49549a04bf70b60bced3d12a
KMS322 <kms930322@naver.com>
2024-03-15 13:33:43 +0900

    constants change

--- front/src/constants.js
+++ front/src/constants.js
@@ -1,4 +1,4 @@
 // export const API_URL = "http://localhost:3060";
 // export const API_URL = "http://www.accydream.com:3060";
-export const API_URL = "localhost:3060";
+export const API_URL = "http://localhost:3060";
 // export const API_URL = "https://www.accydream.com:3060";
