d290dde3dcb9b2d7b3a6450ac082bdb23bbfdf3a
KMS322 <kms930322@naver.com>
2024-03-15 13:20:13 +0900

    secure change

--- back/app.js
+++ back/app.js
@@ -73,7 +73,7 @@ app.use(
     credentials: true,
     cookie: {
       httpOnly: true,
-      secure: false,
+      secure: true,
       // secure: true,
       maxAge: 1000 * 60 * 60 * 24,
     },
