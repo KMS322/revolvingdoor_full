128720b1b5b4c110b24b5d6ee1b65e7573a255e5
KMS322 <kms930322@naver.com>
2025-04-28 14:45:01 +0900

    250428_02

--- admin/src/constants.js
+++ admin/src/constants.js
@@ -1,2 +1,3 @@
 // export const API_URL = "http://localhost:3060";
-export const API_URL = "http://www.accydream.com:3060";
+// export const API_URL = "http://www.accydream.com:3060";
+export const API_URL = "http://175.45.195.45:3060";
