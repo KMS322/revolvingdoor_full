9be6d7ac1c97792cd900e306fed4e0fe15a8f92b
KMS322 <kms930322@naver.com>
2025-04-28 14:56:34 +0900

    250428_03

--- admin/src/component/subHeader.js
+++ admin/src/component/subHeader.js
@@ -1,18 +1,18 @@
 import "../css/subHeader.css";
-// import { useDispatch } from "react-redux";
-// import { LOAD_DUMMY_REQUEST } from "../reducers/adminUser";
+import { useDispatch } from "react-redux";
+import { LOAD_DUMMY_REQUEST } from "../reducers/adminUser";
 const SubHeader = ({ data }) => {
-  // const dispatch = useDispatch();
-  // const addDummy = () => {
-  //   dispatch({
-  //     type: LOAD_DUMMY_REQUEST,
-  //   });
-  // };
+  const dispatch = useDispatch();
+  const addDummy = () => {
+    dispatch({
+      type: LOAD_DUMMY_REQUEST,
+    });
+  };
 
   return (
     <div className="subHeader">
       <div className="box">
-        {/* <img src="/images/subHeader_img.png" alt="" onClick={addDummy} /> */}
+        <img src="/images/subHeader_img.png" alt="" onClick={addDummy} />
         <img src="/images/subHeader_img.png" alt="" />
         <p>페이지 관리자</p>
       </div>
